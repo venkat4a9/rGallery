e87e3a760180c3fc4a50ded448f5ca3679d810a7
cgm <cgm.cgm@gmail.com>
2017-04-25 12:20:50 +0530

    rgallery

---
+++ config/routes.rb
@@ -0,0 +1,30 @@
+Rails.application.routes.draw do
+
+  get "upload" => "images#new", :as => "upload"
+
+  get 'images/new'
+
+  get 'images/create'
+
+  get 'images/destroy'
+
+  get 'images/index'
+
+  get 'categories/new'
+
+  get 'categories/create'
+
+  get 'categories/edit'
+
+  get 'categories/update'
+
+  get 'categories/destroy'
+
+  get 'categories/show'
+
+  resources :images, :categories
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+root 'images#index'
+end
